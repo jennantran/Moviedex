b7252fea90d9ecfa400bff31bf48a236430a2c5f
Jennifer Tran <jennantran@gmail.com>
2020-05-08 15:00:04 -0700

    change back to original server.js file

--- server.js
+++ server.js
@@ -1,15 +1,64 @@
-const express = require('express');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const MOVIES = require('./movies-data-small.json')
 
-const app = express();
+const app = express()
+
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+app.use(helmet())
+app.use(cors())
 
 console.log(process.env);
 
-app.get('/', (req, res) => {
-    res.send('Hello Express!');
-});
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
+})
 
-const PORT = process.env.PORT || 8001
+app.get('/movie', function handleGetMovie(req, res) {
+  let response = MOVIES;
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(movie =>
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    )
+  }
+
+  res.json(response)
+})
+
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 9000
 
 app.listen(PORT, () => {
-    console.log(`Express server is listening on port ${PORT}!`);
-});
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
