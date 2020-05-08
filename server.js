47c884965e6e6be470fc9ab20c3dd161114836e6
Jennifer Tran <jennantran@gmail.com>
2020-05-08 14:48:41 -0700

    testing heroku server

--- server.js
+++ server.js
@@ -1,55 +1,15 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const MOVIES = require('./movies-data-small.json')
+const express = require('express');
 
-console.log(process.env.API_TOKEN)
+const app = express();
 
-const app = express()
+console.log(process.env);
 
-app.use(morgan('dev'))
-app.use(helmet())
-app.use(cors())
+app.get('/', (req, res) => {
+    res.send('Hello Express!');
+});
 
-
-
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401).json({ error: 'Unauthorized request' })
-  }
-  next()
-})
-
-app.get('/movie', function handleGetMovie(req, res) {
-  let response = MOVIES;
-
-  if (req.query.genre) {
-    response = response.filter(movie =>
-      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
-    )
-  }
-
-  if (req.query.country) {
-    response = response.filter(movie =>
-      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
-    )
-  }
-
-  if (req.query.avg_vote) {
-    response = response.filter(movie =>
-      Number(movie.avg_vote) >= Number(req.query.avg_vote)
-    )
-  }
-
-  res.json(response)
-})
-
-const PORT = 9000
+const PORT = process.env.PORT || 8001
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+    console.log('Express server is listening on port 8001!');
+});
